7dede0b115704c9165356fb002060f9b35c9ab13
Emmanuel Maldonado <bugatti26@hotmail.com>
2021-10-14 10:40:42 -0500

    first ts configuration

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+var hello = function (word) {
+    return "Hello " + word;
+};
+exports.hello = hello;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = void 0;
+var hello = function (word) {
+    return "Hello " + word;
+};
+exports.hello = hello;
